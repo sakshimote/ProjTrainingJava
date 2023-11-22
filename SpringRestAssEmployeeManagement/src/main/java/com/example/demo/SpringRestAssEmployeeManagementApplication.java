3abcf6a8ec88ecf9525aed105f72429003fa7bd2
sakshimote <sakshimote19@gmail.com>
2023-11-22 14:50:38 +0530

    latest commit

---
+++ SpringRestAssEmployeeManagement/src/main/java/com/example/demo/SpringRestAssEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestAssEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestAssEmployeeManagementApplication.class, args);
+	}
+
+}
