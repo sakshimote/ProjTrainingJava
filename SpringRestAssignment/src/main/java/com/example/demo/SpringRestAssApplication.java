f627c223bad975916e349db6f822f7da89e64310
sakshimote <sakshimote19@gmail.com>
2023-11-22 10:57:47 +0530

    spring rest ass started

---
+++ SpringRestAssignment/src/main/java/com/example/demo/SpringRestAssApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestAssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestAssApplication.class, args);
+	}
+
+}
